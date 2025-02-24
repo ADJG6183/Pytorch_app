9e7ac900fa6ed7db5f56a4f573f172b373d6efa9
ADJG6183 <aarongreen6183@gmail.com>
2025-02-24 03:32:39 -0500

    app route

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # 1 load the image 
+    # 2 image to tensor 
+    # 3 prediction 
+    # 4 return prediction json
+    return jsonify ({'result': 1})
